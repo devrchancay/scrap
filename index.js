e4483b67e0c3b999db1061fbb9d674dcf5adc98b
Ramon Chancay <ramon.chancay@gmail.com>
2015-10-08 14:45:24 -0500

    add test scraping

---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+let xray = require('x-ray');
+let fs   = require('fs');
+
+
+let x    = new xray();
+let num  = 177;
+
+x(`http://tympanus.net/codrops/collective/collective-${num}`, '.ct-coll-item',[{
+	title : 'h2',
+	content : 'p',
+	img :  'article img@src',
+	url : 'article a@href'
+}])
+.paginate('.ct-coll-nav-bottom-next a@href')
+.limit(10)
+((err,results) => {
+
+
+	if(!err) { 
+		results = results.filter((result) => {
+			return result.title !== undefined;
+		});
+
+		fs.writeFile("./results.json", JSON.stringify(results, null, '\t'));
+	}
+	else{
+		fs.writeFile("./results.json", JSON.stringify(err, null, '\t'));
+	}
+
+});
